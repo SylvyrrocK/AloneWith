a6bda6c567341535914e069e92c54854c43959ef
Shmertalll <kirill.antoshin2016@gmail.com>
2023-07-08 00:16:03 +0300

    Add panic bar and "Prefabs" folder

---
+++ GMTK2023/Assets/Game/Scripts/PanicBar.cs
@@ -0,0 +1,94 @@
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PanicBar : MonoBehaviour
+{
+    [Header("VALUES SETTINGS")]
+    [SerializeField] private float Value = 0f;
+    [SerializeField] private float MaximumValue = 100f;
+    [SerializeField] private float WarningValue = 80f;
+    [SerializeField] private float SmoothTime = 15f;
+
+    [Header("VIEW SETTINGS")]
+    [SerializeField] private Slider BarSlider;
+    [SerializeField] private Image BarBackgroundImage;
+    [SerializeField] private Image BarFillImage;
+    [SerializeField] private GameObject WarningObject;
+
+    private bool FadeInOut = false;
+    private float TimeScale = 1;
+    private float ViewValue = 0f;
+    void Start()
+    {
+        BarSlider.maxValue = MaximumValue;
+    }
+    void Update()
+    {
+      if(Value >= 0f){
+        ChangeVisibility(true);
+        if(ViewValue != Value)
+        {
+        ChangeViewValues();
+        }
+      }else{
+        ChangeVisibility(false);
+      }
+
+      if(Value >= WarningValue){
+      WarningObject.SetActive(true);
+      }else{
+      WarningObject.SetActive(false);
+      }
+      
+    }
+
+//PRIVATE VOID
+    private void ChangeViewValues()
+    {
+       ViewValue = Mathf.Lerp(ViewValue, Value, SmoothTime * 0.5f * Time.deltaTime);
+       BarSlider.value = ViewValue;
+    }
+    private void ChangeVisibility(bool SetVisibility)
+    {
+             
+    }
+
+//PUBLIC VOIDS
+    public void ChangeValueTo(float TargetValue)
+    {
+        if(TargetValue <= MaximumValue)
+        {
+            Value = TargetValue;
+        }
+    }
+
+    public void AddValue(float AddValue)
+    {
+        if(AddValue <= MaximumValue)
+        {
+            Value += AddValue;
+        }
+    }
+    public void ResetValue()
+    {
+        if(Value > 0f)
+        {
+            Value = 0f;
+        }
+    }
+    public void GetValue(string GetType) // ViewValue | Value
+    {
+        if(GetType == "Value")
+        {
+          print(Value);
+        }
+        if(GetType == "ViewValue")
+        {
+          print(ViewValue);
+        }
+      if(GetType != null)
+      {
+        Debug.LogWarning("Get Value type is ViewValue or Value");
+      }
+    }
+}
