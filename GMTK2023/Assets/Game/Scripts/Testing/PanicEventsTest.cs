291d482635918bb4c5aaf2eac00b11f78791ae38
Shmertalll <kirill.antoshin2016@gmail.com>
2023-07-09 15:11:52 +0300

    Update PanicBar fix GetValue issue

--- GMTK2023/Assets/Game/Scripts/Testing/PanicEventsTest.cs
+++ GMTK2023/Assets/Game/Scripts/Testing/PanicEventsTest.cs
@@ -3,18 +3,20 @@ using UnityEngine;
 
 public class PanicEventsTest : MonoBehaviour
 {
+    public PanicBar Bar;
     // Start is called before the first frame update
     void Start()
     {
-        PanicBar.OnPanicValueChanged.AddListener(OnPanicWarning);
+        PanicBar.OnPanicValueChanged.AddListener(OnPanicValueChanged);
         PanicBar.OnPanicWarning.AddListener(OnPanicWarning);
         PanicBar.OnPanicWarningCancel.AddListener(OnPanicWarningCancel);
     }
 
     // Update is called once per frame
-    void OnPanicWarning(float value)
+    void OnPanicValueChanged(float value)
     {
-        Debug.Log("Panic value changed to " + value);
+        Debug.Log("Panic value changed to " + value + "Val bar" + Bar.GetValue("Value"));
+        
     }
     void OnPanicWarning()
     {
